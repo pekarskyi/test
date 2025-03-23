6634afa95c8bac09fef757db9fa4d02af70e4723
Nikolay Pekarskiy <dizer7@gmail.com>
2025-03-23 20:58:34 +0200

    update

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
  * Plugin Name: Test
  * Plugin URI: https://example.com
  * Description: Простий тестовий плагін з сторінкою налаштувань.
- * Version: 1.0
+ * Version: 1.0.0
  * Requires at least: 6.6.2
  * Tested up to:      6.7.2
  * Requires PHP:      7.4
